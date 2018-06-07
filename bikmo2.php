9c78341d025d064df42aeeb280f4340a2a9ee314
Brian Johnston <brian.johnston96@gmail.com>
2018-06-07 14:01:40 +0100

    Update bikmo2.php

--- bikmo2.php
+++ bikmo2.php
@@ -5,7 +5,8 @@
  Along with the other headers I beleive what you have asked me to do here is cache the response so therefore in the cache-control header I've tried to manipulate this to save the response (this could be
  a image like I've tried to implement) for 24 hours. Along with the cache-control header we have the etag header and the expiry header which we can use to cache responses, I do believe the cache-control header overwrites the expiry header though. 
 
- Below I've tried to implement the middleware to do this. It should function by taking the request, creating the response and then sending that back to the client side. I've also tried to this by following the psr-7 and 15 standards and interfaces that come with these. I've tried many resources but cant seem to get the response I wanted from the apache server I have set up at home.
+ Below I've tried to implement the middleware to do this. It should function by taking the request, creating the response and then sending that back to the client side. I've also tried to this by following the psr-7 and 15 standards and interfaces that come with these. 
+I've tried many resources but cant seem to get the response I wanted from the apache server I have set up at home.*/
 
 
 <html>
